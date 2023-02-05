2870d44506ff11ac467bcb4f3244e5a1f6145043
Hua Liang <Hua_Liang@mckinsey.com>
2023-02-05 14:04:26 +0800

    init

---
+++ src/main/java/com/hua/springbatchplayground/SpringbatchPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.hua.springbatchplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringbatchPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hua/springbatchplayground/SpringbatchPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hua.springbatchplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
